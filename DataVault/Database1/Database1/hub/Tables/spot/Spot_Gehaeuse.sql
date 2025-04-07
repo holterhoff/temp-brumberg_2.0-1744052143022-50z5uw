1f33915b9247405fb0fe847a54df49a0970f6697
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:52 +0200

    Copy DataVault/Database1/Database1/hub/Tables/spot/Spot_Gehaeuse.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/spot/Spot_Gehaeuse.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [hub].[Spot_Gehaeuse] (
+    [Gehaeuse_SK] INT          IDENTITY (1, 1) NOT NULL,
+    [Leuchtentyp] VARCHAR (50) NULL,
+    [Typ]         VARCHAR (50) NULL,
+    [Farbe_DE]    VARCHAR (50) NULL,
+    [Farbe_EN]    VARCHAR (50) NULL,
+    [Form_DE]     VARCHAR (50) NULL,
+    [Form_EN]     VARCHAR (50) NULL,
+    CONSTRAINT [PK_hub_Spot_Gehaeusee] PRIMARY KEY CLUSTERED ([Gehaeuse_SK] ASC)
+);
+
+
+
