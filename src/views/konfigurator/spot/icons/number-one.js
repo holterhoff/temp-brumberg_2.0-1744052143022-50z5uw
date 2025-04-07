fdcf75e47ff215f5e67017482948d36f07b5fd3d
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:43:34 +0200

    Copy src/views/konfigurator/spot/icons/number-one.js from brumberg_2.0

---
+++ src/views/konfigurator/spot/icons/number-one.js
@@ -0,0 +1,9 @@
+import * as React from 'react'
+
+const SvgNumberOne = props => (
+  <svg height='1em' viewBox='0 0 512 512' width='1em' xmlns='http://www.w3.org/2000/svg' data-name='Layer 1' {...props}>
+    <path d='M256 6C117.929 6 6 117.929 6 256s111.929 250 250 250 250-111.929 250-250S394.071 6 256 6zm52.075 374.689h-67.182V187.824h-36.968v-56.513h104.15z' />
+  </svg>
+)
+
+export default SvgNumberOne
