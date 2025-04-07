6c636c9f3db24377765b18f5a23bff981e20fab1
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:28:32 +0200

    Copy src/pages/login/index.js from brumberg_2.0

---
+++ src/pages/login/index.js
@@ -0,0 +1,326 @@
+// ** React Imports
+import { useState } from 'react'
+
+// ** Next Imports
+import Link from 'next/link'
+
+// ** MUI Components
+import Alert from '@mui/material/Alert'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import Checkbox from '@mui/material/Checkbox'
+import CardContent from '@mui/material/CardContent'
+import TextField from '@mui/material/TextField'
+import InputLabel from '@mui/material/InputLabel'
+import IconButton from '@mui/material/IconButton'
+import Box from '@mui/material/Box'
+import FormControl from '@mui/material/FormControl'
+import useMediaQuery from '@mui/material/useMediaQuery'
+import OutlinedInput from '@mui/material/OutlinedInput'
+import { styled, useTheme } from '@mui/material/styles'
+import FormHelperText from '@mui/material/FormHelperText'
+import MuiCard from '@mui/material/Card'
+import CardMedia from '@mui/material/CardMedia';
+import InputAdornment from '@mui/material/InputAdornment'
+import Typography from '@mui/material/Typography'
+import MuiFormControlLabel from '@mui/material/FormControlLabel'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Third Party Imports
+import * as yup from 'yup'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import nProgress from 'nprogress'
+
+
+// ** Hooks
+import { useAuth } from 'src/hooks/useAuth'
+import useBgColor from 'src/@core/hooks/useBgColor'
+import { useSettings } from 'src/@core/hooks/useSettings'
+
+// ** Configs
+import themeConfig from 'src/configs/themeConfig'
+
+// ** Layout Import
+import BlankLayout from 'src/@core/layouts/BlankLayout'
+
+// ** Demo Imports
+import FooterIllustrationsV1 from 'src/views/pages/auth/FooterIllustrationsV1'
+
+// ** Styled Components
+const Card = styled(MuiCard)(({ theme }) => ({
+  [theme.breakpoints.up('sm')]: { width: 450 }
+}))
+
+const LoginIllustrationWrapper = styled(Box)(({ theme }) => ({
+  padding: theme.spacing(20),
+  paddingRight: '0 !important',
+  [theme.breakpoints.down('lg')]: {
+    padding: theme.spacing(10)
+  }
+}))
+
+const LoginIllustration = styled('img')(({ theme }) => ({
+  maxWidth: '48rem',
+  [theme.breakpoints.down('xl')]: {
+    maxWidth: '38rem'
+  },
+  [theme.breakpoints.down('lg')]: {
+    maxWidth: '30rem'
+  }
+}))
+
+const RightWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  [theme.breakpoints.up('md')]: {
+    maxWidth: 400
+  },
+  [theme.breakpoints.up('lg')]: {
+    maxWidth: 450
+  }
+}))
+
+const BoxWrapper = styled(Box)(({ theme }) => ({
+  width: '100%',
+  [theme.breakpoints.down('md')]: {
+    maxWidth: 400
+  }
+}))
+
+const TypographyStyled = styled(Typography)(({ theme }) => ({
+  fontWeight: 600,
+  letterSpacing: '0.18px',
+  marginBottom: theme.spacing(1.5),
+  [theme.breakpoints.down('md')]: { marginTop: theme.spacing(8) }
+}))
+
+const FormControlLabel = styled(MuiFormControlLabel)(({ theme }) => ({
+  '& .MuiFormControlLabel-label': {
+    fontSize: '0.875rem',
+    color: theme.palette.text.secondary
+  }
+}))
+
+const schema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().min(5).required()
+})
+
+const defaultValues = {
+  //password: 'admin',
+  //email: 'admin@materialize.com'
+  password: '',
+  email: ''
+}
+
+const LoginPage = () => {
+  // ** State
+
+  const [values, setValues] = useState({
+    password: '',
+    showPassword: false
+  })
+  const [rememberMe, setRememberMe] = useState(true)
+  const [showPassword, setShowPassword] = useState(false)
+
+  // ** Hooks
+  const auth = useAuth()
+  const theme = useTheme()
+  const bgColors = useBgColor()
+  const { settings } = useSettings()
+  const hidden = useMediaQuery(theme.breakpoints.down('md'))
+
+  // ** Vars
+  const { skin } = settings
+
+  const {
+    control,
+    setError,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues,
+    mode: 'onBlur',
+    resolver: yupResolver(schema)
+  })
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value })
+  }
+
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword })
+  }
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault()
+  }
+
+  const onSubmit = data => {
+
+    auth.setLoading(() => {
+      nProgress.start()
+    } )
+
+    const { email, password } = data
+    auth.login({ email, password, rememberMe }, () => {
+      nProgress.done()
+      setError('email', {
+        type: 'manual',
+        message: 'Email or Password is invalid'
+      })
+    })
+  }
+  const imageSource = skin === 'bordered' ? 'auth-v2-login-illustration-bordered' : 'auth-v2-login-illustration'
+
+
+  //nProgress.start()
+
+
+  return (
+    <Box className='content-center'>
+      <Card sx={{ zIndex: 1 }}>
+        <CardContent sx={{ p: theme => `${theme.spacing(13, 7, 6.5)} !important` }}>
+          <Box sx={{ mb: 8, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+            <CardMedia
+              component='img'
+              sx={{ width: '80%' }}
+              image='https://www.brumberg.com/fileadmin/mediafiles/brumberg_logo.svg'
+              alt='Brumberg Logo'
+            />
+            {/* <Typography variant='h6' sx={{ ml: 2, lineHeight: 1, fontWeight: 700, fontSize: '1.5rem !important' }}>
+              {themeConfig.templateName}
+            </Typography> */}
+          </Box>
+          <Box sx={{ mb: 6 }}>
+            <Typography variant='h5' sx={{ mb: 1.5, fontWeight: 600, letterSpacing: '0.18px' }}>
+              {`Welcome to our ${themeConfig.templateName}! 👋🏻`}
+            </Typography>
+            <Typography variant='body2'>Please sign-in to your account</Typography>
+          </Box>
+          <form noValidate autoComplete='off' onSubmit={handleSubmit(onSubmit)}>
+            <FormControl fullWidth sx={{ mb: 4 }}>
+              <Controller
+                name='email'
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <TextField
+                    autoFocus
+                    label='Email'
+                    value={value}
+                    onBlur={onBlur}
+                    onChange={onChange}
+                    error={Boolean(errors.email)}
+                    placeholder='admin@brumberg.de'
+                  />
+                )}
+              />
+              {errors.email && <FormHelperText sx={{ color: 'error.main' }}>{errors.email.message}</FormHelperText>}
+            </FormControl>
+            <FormControl fullWidth>
+              <InputLabel htmlFor='auth-login-password'>Password</InputLabel>
+              <Controller
+                name='password'
+                control={control}
+                rules={{ required: true }}
+                render={({ field: { value, onChange, onBlur } }) => (
+                  <OutlinedInput
+                    value={value}
+                    onBlur={onBlur}
+                    label='Password'
+                    onChange={onChange}
+                    id='auth-login-v1-password'
+                    error={Boolean(errors.password)}
+                    type={showPassword ? 'text' : 'password'}
+                    endAdornment={
+                      <InputAdornment position='end'>
+                        <IconButton
+                          edge='end'
+                          onMouseDown={e => e.preventDefault()}
+                          onClick={() => setShowPassword(!showPassword)}
+                        >
+                          <Icon icon={showPassword ? 'mdi:eye-outline' : 'mdi:eye-off-outline'} fontSize={20} />
+                        </IconButton>
+                      </InputAdornment>
+                    }
+                  />
+                )}
+              />
+              {errors.password && (
+                <FormHelperText sx={{ color: 'error.main' }} id=''>
+                  {errors.password.message}
+                </FormHelperText>
+              )}
+            </FormControl>
+            <Box
+              sx={{ mb: 4, display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'space-between' }}
+            >
+              <FormControlLabel
+                label='Remember Me'
+                control={<Checkbox />}
+                sx={{ '& .MuiFormControlLabel-label': { color: 'text.primary' } }}
+              />
+              <Typography
+                variant='body2'
+                component={Link}
+                href='/pages/auth/forgot-password-v1'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
+              >
+                Forgot Password?
+              </Typography>
+            </Box>
+            <Button fullWidth size='large' type='submit' variant='contained' sx={{ mb: 7 }}>
+              Login
+            </Button>
+            <Box sx={{ display: 'flex', alignItems: 'center', flexWrap: 'wrap', justifyContent: 'center' }}>
+              <Typography sx={{ mr: 2, color: 'text.secondary' }}>New on our platform?</Typography>
+              <Typography
+                component={Link}
+                href='/pages/auth/register-v1'
+                sx={{ color: 'primary.main', textDecoration: 'none' }}
+              >
+                Create an account
+              </Typography>
+            </Box>
+            <Divider
+              sx={{
+                '& .MuiDivider-wrapper': { px: 4 },
+                mt: theme => `${theme.spacing(5)} !important`,
+                mb: theme => `${theme.spacing(7.5)} !important`
+              }}
+            >
+              or
+            </Divider>
+            <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+              <IconButton href='/' component={Link} sx={{ color: '#497ce2' }} onClick={e => e.preventDefault()}>
+                <Icon icon='mdi:facebook' />
+              </IconButton>
+              <IconButton href='/' component={Link} sx={{ color: '#1da1f2' }} onClick={e => e.preventDefault()}>
+                <Icon icon='mdi:twitter' />
+              </IconButton>
+              <IconButton
+                href='/'
+                component={Link}
+                onClick={e => e.preventDefault()}
+                sx={{ color: theme => (theme.palette.mode === 'light' ? '#272727' : 'grey.300') }}
+              >
+                <Icon icon='mdi:github' />
+              </IconButton>
+              <IconButton href='/' component={Link} sx={{ color: '#db4437' }} onClick={e => e.preventDefault()}>
+                <Icon icon='mdi:google' />
+              </IconButton>
+            </Box>
+          </form>
+        </CardContent>
+      </Card>
+      <FooterIllustrationsV1 />
+    </Box>
+  )
+}
+LoginPage.getLayout = page => <BlankLayout>{page}</BlankLayout>
+LoginPage.guestGuard = true
+
+export default LoginPage
