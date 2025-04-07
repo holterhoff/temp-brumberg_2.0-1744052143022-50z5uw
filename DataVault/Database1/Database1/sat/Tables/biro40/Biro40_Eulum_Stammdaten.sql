3e84ffff81eca39c115a34ccd26fae63cc6c1d5e
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:23 +0200

    Copy DataVault/Database1/Database1/sat/Tables/biro40/Biro40_Eulum_Stammdaten.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/biro40/Biro40_Eulum_Stammdaten.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE [sat].[Biro40_Eulum_Stammdaten] (
+    [Eulum_SK]            INT           NOT NULL,
+    [Profilschnitt]       INT           NULL,
+    [Optikschnitt]        INT           NULL,
+    [LS/m]                INT           NULL,
+    [LS/m INDI]           VARCHAR (512) NULL,
+    [W/m]                 INT           NULL,
+    [W/m INDI]            VARCHAR (512) NULL,
+    [Ausschreibungstext]  VARCHAR (512) NULL,
+    [Netzspannung AC]     VARCHAR (512) NULL,
+    [Netzspannung DC]     VARCHAR (512) NULL,
+    [Farbtoleranz]        VARCHAR (512) NULL,
+    [Farbwiedergabeindex] VARCHAR (512) NULL,
+    [Lebensdauer]         VARCHAR (512) NULL,
+    [Schutzklasse]        VARCHAR (512) NULL,
+    [Schutzart]           VARCHAR (512) NULL,
+    [Blendbewertung]      VARCHAR (512) NULL,
+    PRIMARY KEY CLUSTERED ([Eulum_SK] ASC)
+);
+
