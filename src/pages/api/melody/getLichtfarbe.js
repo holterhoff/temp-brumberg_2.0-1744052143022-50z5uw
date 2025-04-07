e8c5300fc794f6536af4094b0bc8b262b22cadac
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:25:03 +0200

    Copy src/pages/api/melody/getLichtfarbe.js from brumberg_2.0

---
+++ src/pages/api/melody/getLichtfarbe.js
@@ -0,0 +1,46 @@
+import query from '../dbservice'
+
+export default async function getLichtfarbe(req, res) {
+  if (req.method == 'POST') {
+    let params = JSON.parse(req.body)
+
+    let sql = 'SELECT DISTINCT Lichtfarbe as [value] FROM [Brumberg].[fakt].[melody]  WHERE 1=1 '
+
+    params.forEach(element => {
+      if (element.label == 'Leuchtentyp') {
+        let leuchtentyp = element.value
+        sql += " AND Leuchtentyp LIKE '%" + leuchtentyp + "%'"
+      }
+
+      if (element.label == 'Gehäusefarbe') {
+        let farbe = element.value
+        sql += " AND Gehaeusefarbe LIKE '%" + farbe + "%'"
+      }
+
+      // if (element.label == 'Farbkanal') {
+      //   let farbkanal = element.value
+      //   sql += " AND Farbkanal LIKE '%" + farbkanal + "%'"
+      // }
+
+      // if (element.label == 'Lichtfarbe') {
+      //   let lichtfarbe = element.value
+      //   sql += " AND Lichtfarbe LIKE '%" + lichtfarbe + "%'"
+      // }
+
+      // if (element.label == 'Leistung') {
+      //   let leistung = element.value
+      //   sql += " AND Leistung LIKE '%" + leistung + "%'"
+      // }
+    })
+
+    const result = await query(sql)
+
+    if (result) {
+      res.status(200).json(result)
+    } else {
+      res.status(500).json({ message: 'Fehler beim Login' })
+    }
+  } else {
+    res.status(401).json({ message: 'This Methode not allowed' })
+  }
+}
