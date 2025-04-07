f9f6b7bc56cd144c2fbc7b4e8e629d773a9e3579
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:30 +0200

    Copy DataVault/Database1/Database1/sat/Tables/led/LED_Stammdaten.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/led/LED_Stammdaten.sql
@@ -0,0 +1,21 @@
+﻿CREATE TABLE [sat].[LED_Stammdaten] (
+    [Leuchte_SK]             INT            NOT NULL,
+    [Image]                  VARCHAR (512)  NULL,
+    [Produktbeschreibung_DE] VARCHAR (MAX)  NULL,
+    [Produktbeschreibung_EN] VARCHAR (MAX)  NULL,
+    [SEO_Url_DE]             VARCHAR (MAX)  NULL,
+    [SEO_Url_EN]             VARCHAR (MAX)  NULL,
+    [Artikelnummer]          INT            NULL,
+    [Preis]                  DECIMAL (5, 2) NULL,
+    [Nettogewicht]           DECIMAL (5, 2) NULL,
+    [Außendurchmesser]       INT            NULL,
+    [Hoehe]                  INT            NULL,
+    [Moegliche_Konverter]    VARCHAR (MAX)  NULL,
+    [Datenblatt_DE]          VARCHAR (MAX)  NULL,
+    [Datenblatt_EN]          VARCHAR (MAX)  NULL,
+    [Status]                 VARCHAR (50)   NULL,
+    PRIMARY KEY CLUSTERED ([Leuchte_SK] ASC)
+);
+
+
+
