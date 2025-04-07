c7956af22939c5dfae21036f436cec4a5787f06a
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:23:40 +0200

    Copy src/pages/api/birocircle/getSchaltungsart.js from brumberg_2.0

---
+++ src/pages/api/birocircle/getSchaltungsart.js
@@ -0,0 +1,46 @@
+import query from '../dbservice'
+
+export default async function getSchaltungsart(req, res) {
+  if (req.method == 'POST') {
+    let params = JSON.parse(req.body)
+
+    let sql = 'SELECT DISTINCT Schaltungsart as [value] FROM [Brumberg].[fakt].[birocircle]  WHERE 1=1 '
+
+    params.forEach(element => {
+      if (element.label == 'Leuchtentyp') {
+        let leuchtentyp = element.value
+        sql += " AND Leuchtentyp LIKE '%" + leuchtentyp + "%'"
+      }
+
+      if (element.label == 'Durchmesser') {
+        let druchmesser = element.value
+        sql += " AND Durchmesser = '" + druchmesser + "'"
+      }
+
+      if (element.label == 'Lichtfarbe') {
+        let lichtfarbe = element.value
+        sql += " AND Lichtfarbe = '" + lichtfarbe + "'"
+      }
+
+      // if (element.label == 'Lichtfarbe') {
+      //   let lichtfarbe = element.value
+      //   sql += " AND Lichtfarbe LIKE '%" + lichtfarbe + "%'"
+      // }
+
+      // if (element.label == 'Leistung') {
+      //   let leistung = element.value
+      //   sql += " AND Leistung LIKE '%" + leistung + "%'"
+      // }
+    })
+
+    const result = await query(sql)
+
+    if (result) {
+      res.status(200).json(result)
+    } else {
+      res.status(500).json({ message: 'Fehler beim Login' })
+    }
+  } else {
+    res.status(401).json({ message: 'This Methode not allowed' })
+  }
+}
