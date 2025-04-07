85f712c9474da45557110b3d0bb68fb0d41a1264
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:04:54 +0200

    Copy DataVault/Database1/Database1/dbo/Stored Procedures/load_link_Biro40_Eulum__GT.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/dbo/Stored Procedures/load_link_Biro40_Eulum__GT.sql
@@ -0,0 +1,15 @@
+﻿
+CREATE PROCEDURE [dbo].[load_link_Biro40_Eulum__GT] as
+
+TRUNCATE TABLE link.Biro40_Eulum__GT
+
+INSERT INTO link.Biro40_Eulum__GT (Eulum_SK, GT_SK)
+
+SELECT 
+
+		Eulum_SK
+	,	GT_SK
+
+FROM hub.Biro40_Eulum Eulum
+inner join hub.Biro40_GT gt ON gt.Lichtrichtung = Eulum.Lichtrichtung and gt.Lichtfarbe = Eulum.Lichtfarbe and gt.Schaltungsart = Eulum.Schaltungsart and (gt.Step % Eulum.Step = 0 )  and (gt.Step BETWEEN Eulum.[Min] and Eulum.[Max]) 
+or gt.Step = 90 and gt.Lichtrichtung = eulum.Lichtrichtung and gt.Lichtfarbe = Eulum.Lichtfarbe and gt.Schaltungsart = Eulum.Schaltungsart
