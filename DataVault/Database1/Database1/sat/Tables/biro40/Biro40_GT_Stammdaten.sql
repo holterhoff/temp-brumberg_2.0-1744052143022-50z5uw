fdb137e331ba2eddcf3ee63f25f097597b7133ee
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:25 +0200

    Copy DataVault/Database1/Database1/sat/Tables/biro40/Biro40_GT_Stammdaten.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/biro40/Biro40_GT_Stammdaten.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [sat].[Biro40_GT_Stammdaten] (
+    [GT_SK]         INT           NOT NULL,
+    [Bezeichnung]   VARCHAR (512) NULL,
+    [Artikelnummer] VARCHAR (512) NULL,
+    [Preis]         VARCHAR (512) NULL,
+    [Watt]          VARCHAR (512) NULL,
+    [Laenge]        VARCHAR (512) NULL,
+    [Anzahl_VG]     VARCHAR (512) NULL,
+    PRIMARY KEY CLUSTERED ([GT_SK] ASC)
+);
+
