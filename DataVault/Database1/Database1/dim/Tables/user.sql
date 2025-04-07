dc530c2d25c99b12f48fc67e22047a151697f1fe
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:29 +0200

    Copy DataVault/Database1/Database1/dim/Tables/user.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/dim/Tables/user.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[user] (
+    [id]          INT  NULL,
+    [fullName]    TEXT NULL,
+    [company]     TEXT NULL,
+    [role]        TEXT NULL,
+    [username]    TEXT NULL,
+    [country]     TEXT NULL,
+    [contact]     TEXT NULL,
+    [email]       TEXT NULL,
+    [password]    TEXT NULL,
+    [currentPlan] TEXT NULL,
+    [status]      TEXT NULL,
+    [avatar]      TEXT NULL,
+    [avatarColor] TEXT NULL
+);
+
