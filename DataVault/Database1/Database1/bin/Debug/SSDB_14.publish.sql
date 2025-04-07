3d97bb6cc2c1644430be947da305c547d5ec93a6
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:01:59 +0200

    Copy DataVault/Database1/Database1/bin/Debug/SSDB_14.publish.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/bin/Debug/SSDB_14.publish.sql
@@ -0,0 +1,130 @@
+﻿/*
+Bereitstellungsskript für Brumberg
+
+Dieser Code wurde von einem Tool generiert.
+Änderungen an dieser Datei führen möglicherweise zu falschem Verhalten und gehen verloren, falls
+der Code neu generiert wird.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Brumberg"
+:setvar DefaultFilePrefix "Brumberg"
+:setvar DefaultDataPath ""
+:setvar DefaultLogPath ""
+
+GO
+:on error exit
+GO
+/*
+Überprüfen Sie den SQLCMD-Modus, und deaktivieren Sie die Skriptausführung, wenn der SQLCMD-Modus nicht unterstützt wird.
+Um das Skript nach dem Aktivieren des SQLCMD-Modus erneut zu aktivieren, führen Sie folgenden Befehl aus:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'Der SQLCMD-Modus muss aktiviert sein, damit dieses Skript erfolgreich ausgeführt werden kann.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+PRINT N'Tabelle "[sat].[Biro40_Leuchte_Artikel_MA]" wird erstellt...';
+
+
+GO
+CREATE TABLE [sat].[Biro40_Leuchte_Artikel_MA] (
+    [Leuchte_SK]    INT           NOT NULL,
+    [Mass]          INT           NOT NULL,
+    [Artikelnummer] NVARCHAR (50) NOT NULL
+);
+
+
+GO
+PRINT N'Tabelle "[sat].[Biro40_Leuchte_Laengen]" wird erstellt...';
+
+
+GO
+CREATE TABLE [sat].[Biro40_Leuchte_Laengen] (
+    [Leuchte_SK] INT NOT NULL,
+    [Min]        INT NOT NULL,
+    [Max]        INT NOT NULL,
+    [Step]       INT NOT NULL,
+    [Toleranz]   INT NOT NULL,
+    [Abzug]      INT NOT NULL,
+    PRIMARY KEY CLUSTERED ([Leuchte_SK] ASC)
+);
+
+
+GO
+PRINT N'Prozedur "[dbo].[sat_hub_Biro40_Leuchte_Artikel_MA]" wird erstellt...';
+
+
+GO
+CREATE PROCEDURE [dbo].[sat_hub_Biro40_Leuchte_Artikel_MA] as
+
+TRUNCATE TABLE sat.Biro40_Leuchte_Artikel_MA;
+
+WITH cte as
+(
+	SELECT Leuchte_SK, [min] , [step], [max], [min] as newval  FROM sat.Biro40_Leuchte_Laengen
+
+	UNION ALL
+
+	SELECT Leuchte_SK, [min] , [step], [max],  newval + [step] from cte
+	WHERE newval < [max]
+
+)
+INSERT INTO sat.Biro40_Leuchte_Artikel_MA (Leuchte_SK, Mass, Artikelnummer)
+SELECT Leuchte_SK, newval as Mass, NULL AS Artikelnummer  FROM cte
+
+order by Leuchte_SK, Mass
+GO
+PRINT N'Prozedur "[dbo].[sat_hub_Biro40_Leuchte_Laengen]" wird erstellt...';
+
+
+GO
+CREATE PROCEDURE [dbo].[sat_hub_Biro40_Leuchte_Laengen] as
+
+TRUNCATE TABLE sat.Biro40_Leuchte_Laengen
+
+INSERT INTO sat.Biro40_Leuchte_Laengen(Leuchte_SK, [Min], [Max], [Step], [Toleranz], [Abzug])
+SELECT * FROM (VALUES
+
+
+ (1,  560	    ,  2380 	, 140	    , 2	, 591)
+,(2,  560	    ,  2380 	, 140	    , 2	, 591)
+,(3,  840	    ,  2380 	, 140	    , 2	, 591)
+,(4,  840	    ,  2380 	, 140	    , 2	, 591)
+,(5,  980	    ,  2520 	, 70	    , 2	, 591)
+,(6,  2520  	,  5600 	, 280	    , 2	, 591)
+,(7,  560	    ,  2240 	, 140	    , 2	, 591)
+,(8,  560	    ,  2240 	, 140	    , 2	, 591)
+,(9,  560	    ,  2240 	, 140	    , 2	, 591)
+,(10, 560	    ,  2240 	, 140	    , 2	, 591)
+,(11, 2520  	,  2520 	, 2520  	, 2	, 591)
+,(12, 1050  	,  1050 	, 1050  	, 2	, 591)
+,(13, 1120  	,  1120 	, 1120  	, 2	, 591)
+,(14, 1190  	,  1190 	, 1190  	, 2	, 591)
+,(15, 1260  	,  1260 	, 1260  	, 2	, 591)
+,(16, 1330  	,  1330 	, 1330  	, 2	, 591)
+
+
+
+)t (Leuchte_SK ,[Min], [Max], [Step], [Toleranz], [Abzug])
+GO
+PRINT N'Update abgeschlossen.';
+
+
+GO
