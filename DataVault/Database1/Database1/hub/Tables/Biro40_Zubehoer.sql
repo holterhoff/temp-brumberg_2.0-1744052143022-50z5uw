394938f66c4c4c3aa483d8e49cd902ca096e6af1
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:39 +0200

    Copy DataVault/Database1/Database1/hub/Tables/Biro40_Zubehoer.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/Biro40_Zubehoer.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [hub].[Biro40_Zubehoer] (
+    [Zubehoer_SK]    INT           IDENTITY (1, 1) NOT NULL,
+    [Typ]            VARCHAR (512) NULL,
+    [Montageart]     VARCHAR (512) NULL,
+    [Lichtpunkthöhe] VARCHAR (512) NULL,
+    [Lichtrichtung]  VARCHAR (512) NULL,
+    [Gehaeusefarbe]  VARCHAR (512) NULL,
+    [Form]           VARCHAR (512) NULL,
+    CONSTRAINT [PK_hub_Biro40_Zubehoer] PRIMARY KEY CLUSTERED ([Zubehoer_SK] ASC)
+);
+
