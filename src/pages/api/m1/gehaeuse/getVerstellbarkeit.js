2761b98dfd5c51046431032a2a0519a1801a0ed5
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:24:08 +0200

    Copy src/pages/api/m1/gehaeuse/getVerstellbarkeit.js from brumberg_2.0

---
+++ src/pages/api/m1/gehaeuse/getVerstellbarkeit.js
@@ -0,0 +1,46 @@
+'use server'
+
+import { neon } from '@neondatabase/serverless'
+
+const sql = neon(process.env.DATABASE_URL)
+export default async function getVerstellbarkeit(req, res) {
+  if (req.method == 'POST') {
+    let params = JSON.parse(req.body)
+
+    let query = 'SELECT DISTINCT verstellbarkeit_de as value_de, verstellbarkeit_en as value_en FROM m1  WHERE 1=1 '
+
+    params.forEach(element => {
+      if (element.label == 'Form') {
+        let form = element.value
+        query += " AND (form_de = '" + form + "' OR form_en = '" + form + "')"
+      }
+      if (element.label == 'Leuchtentyp') {
+        let typ = element.value
+        query += " AND (typ_de = '" + typ + "' OR typ_en = '" + typ + "')"
+      }
+      if (element.label == 'Anzahl Leuchtmittel') {
+        let anzahl_leuchtmittel = element.value
+        query +=
+          " AND (anzahl_leuchtmittel = '" +
+          anzahl_leuchtmittel +
+          "' OR anzahl_leuchtmittel = '" +
+          anzahl_leuchtmittel +
+          "')"
+      }
+      if (element.label == 'Farbe') {
+        let farbe = element.value
+        query += " AND (farbe_de = '" + farbe + "' OR farbe_en = '" + farbe + "')"
+      }
+    })
+
+    const result = await sql(query)
+
+    if (result) {
+      res.status(200).json(result)
+    } else {
+      res.status(500).json({ message: 'Fehler beim Login' })
+    }
+  } else {
+    res.status(401).json({ message: 'This Methode not allowed' })
+  }
+}
