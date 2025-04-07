4fae355944d2db960f6fdcb3b2bf9b7605313cc7
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:27 +0200

    Copy DataVault/Database1/Database1/dbo/Tables/users.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/dbo/Tables/users.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[users] (
+    [id]         INT  NULL,
+    [username]   TEXT NULL,
+    [fullName]   TEXT NULL,
+    [street]     TEXT NULL,
+    [zip]        TEXT NULL,
+    [city]       TEXT NULL,
+    [country]    TEXT NULL,
+    [phone]      TEXT NULL,
+    [fax]        TEXT NULL,
+    [email]      TEXT NULL,
+    [role]       TEXT NULL,
+    [web]        TEXT NULL,
+    [password]   TEXT NULL,
+    [avatar]     TEXT NULL,
+    [logo]       TEXT NULL,
+    [status]     TEXT NULL,
+    [language]   TEXT NULL,
+    [expireDate] TEXT NULL,
+    [faktor]     INT  NULL,
+    [tax]        INT  NULL
+);
+
