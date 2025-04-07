0447477376f308e7fa38d39584c4752856192d9c
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:41:28 +0200

    Copy src/views/konfigurator/biro40/icons/Installation.js from brumberg_2.0

---
+++ src/views/konfigurator/biro40/icons/Installation.js
@@ -0,0 +1,92 @@
+import * as React from 'react'
+
+const SvgInstallation = props => (
+  <svg
+    xmlSpace='preserve'
+    width='1em'
+    height='1em'
+    viewBox='0 0 682.667 682.667'
+    xmlns='http://www.w3.org/2000/svg'
+    {...props}
+  >
+    <defs>
+      <clipPath clipPathUnits='userSpaceOnUse' id='installation_svg__a'>
+        <path d='M0 512h512V0H0Z' />
+      </clipPath>
+    </defs>
+    <g clipPath='url(#installation_svg__a)' transform='matrix(1.33333 0 0 -1.33333 0 682.667)'>
+      <path
+        d='M0 0a205.21 205.21 0 0 0-38.778-14.23v-21.884c0-10.432-8.561-18.965-18.993-18.965h-63.411c-10.46 0-18.992 8.533-18.992 18.965v21.884a203.887 203.887 0 0 0-53.603 22.224l-15.477-15.477c-7.399-7.399-19.475-7.399-26.845 0l-44.844 44.845c-7.398 7.37-7.398 19.474 0 26.845l15.478 15.478a203.88 203.88 0 0 0-22.224 53.604h-21.884c-10.431 0-18.964 8.561-18.964 18.993v63.413c0 10.46 8.533 18.992 18.964 18.992h21.884a203.886 203.886 0 0 0 22.224 53.605l-15.478 15.478c-7.398 7.398-7.398 19.475 0 26.845l44.844 44.845c7.37 7.399 19.446 7.399 26.845 0l15.477-15.478a203.855 203.855 0 0 0 53.603 22.225v21.884c0 10.432 8.532 18.964 18.992 18.964h63.411c10.432 0 18.993-8.532 18.993-18.964v-21.884a203.855 203.855 0 0 0 53.603-22.225l15.477 15.478c7.37 7.399 19.474 7.399 26.844 0l44.845-44.845c7.398-7.37 7.398-19.447 0-26.845l-15.477-15.478a203.883 203.883 0 0 0 22.223-53.605h21.884c10.431 0 18.963-8.532 18.963-18.992v-63.413c0-8.448-5.584-15.648-13.237-18.086'
+        style={{
+          fill: 'none',
+          stroke: '#000',
+          strokeWidth: 15,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(336.037 72.012)'
+      />
+      <path
+        d='M0 0c-30.444-26.278-49.692-65.142-49.692-108.514 0-79.202 64.177-143.381 143.349-143.381 23.981 0 46.545 5.869 66.387 16.272m74.268 99.159a143.995 143.995 0 0 1 2.722 27.95c0 79.174-64.177 143.353-143.377 143.353-24.322 0-47.225-6.067-67.295-16.754'
+        style={{
+          fill: 'none',
+          stroke: '#000',
+          strokeWidth: 15,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(152.89 364.528)'
+      />
+      <path
+        d='M0 0v91.278c-29.934 11.084-51.393 39.97-51.393 73.618 0 26.874 13.664 50.714 34.414 64.859C4.791 244.638.567 221.138.567 217.197v-47.141c0-16.498 12.926-30.105 29.14-31.211 16.214 1.106 29.14 14.713 29.14 31.211v47.141c0 3.941-4.195 27.441 17.575 12.558 20.75-14.145 34.413-37.985 34.413-64.859 0-33.648-21.459-62.534-51.393-73.618V0'
+        style={{
+          fill: 'none',
+          stroke: '#000',
+          strokeWidth: 15,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(216.84 115.723)'
+      />
+      <path
+        d='M0 0c-13.437-7.172-28.8-11.254-45.1-11.254-52.951 0-95.867 42.918-95.867 95.871 0 52.952 42.916 95.87 95.867 95.87 52.952 0 95.869-42.918 95.869-95.87 0-24.464-9.185-46.773-24.265-63.725'
+        style={{
+          fill: 'none',
+          stroke: '#000',
+          strokeWidth: 15,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(453.731 49.986)'
+      />
+      <path
+        d='m0 0 30.954 36.908c6.435 7.682 17.943 8.703 25.654 2.268 7.682-6.435 8.702-17.944 2.239-25.654l-42.69-50.94c-3.231-3.884-7.398-6.039-12.415-6.463-5.018-.426-9.468.992-13.324 4.252l-24.009 20.381c-7.654 6.492-8.589 18.001-2.126 25.655 6.491 7.654 18.029 8.589 25.654 2.126z'
+        style={{
+          fill: 'none',
+          stroke: '#000',
+          strokeWidth: 15,
+          strokeLinecap: 'round',
+          strokeLinejoin: 'round',
+          strokeMiterlimit: 10,
+          strokeDasharray: 'none',
+          strokeOpacity: 1
+        }}
+        transform='translate(397.719 135.623)'
+      />
+    </g>
+  </svg>
+)
+
+export default SvgInstallation
