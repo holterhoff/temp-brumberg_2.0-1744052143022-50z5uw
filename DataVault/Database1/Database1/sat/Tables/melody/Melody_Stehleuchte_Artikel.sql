61aed77b9f74bfe26f84c9c17f594662207aa5a5
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:32 +0200

    Copy DataVault/Database1/Database1/sat/Tables/melody/Melody_Stehleuchte_Artikel.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/melody/Melody_Stehleuchte_Artikel.sql
@@ -0,0 +1,7 @@
+CREATE TABLE [sat].[Melody_Stehleuchte_Artikel] (
+    [Melody_SK]  INT           NOT NULL,
+    [Artikelnummer] NVARCHAR(50) NOT NULL,
+    [Preis] DECIMAL(10,2) NOT NULL
+    PRIMARY KEY CLUSTERED ([Melody_SK] ASC)
+);
+
