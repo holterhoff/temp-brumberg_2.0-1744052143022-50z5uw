90d7bef957993858c54bd375f1bcea501421a17a
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:03:38 +0200

    Copy DataVault/Database1/Database1/bin/Debug/SSDB_66.publish.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/bin/Debug/SSDB_66.publish.sql
@@ -0,0 +1,152 @@
+﻿/*
+Bereitstellungsskript für Brumberg
+
+Dieser Code wurde von einem Tool generiert.
+Änderungen an dieser Datei führen möglicherweise zu falschem Verhalten und gehen verloren, falls
+der Code neu generiert wird.
+*/
+
+GO
+SET ANSI_NULLS, ANSI_PADDING, ANSI_WARNINGS, ARITHABORT, CONCAT_NULL_YIELDS_NULL, QUOTED_IDENTIFIER ON;
+
+SET NUMERIC_ROUNDABORT OFF;
+
+
+GO
+:setvar DatabaseName "Brumberg"
+:setvar DefaultFilePrefix "Brumberg"
+:setvar DefaultDataPath ""
+:setvar DefaultLogPath ""
+
+GO
+:on error exit
+GO
+/*
+Überprüfen Sie den SQLCMD-Modus, und deaktivieren Sie die Skriptausführung, wenn der SQLCMD-Modus nicht unterstützt wird.
+Um das Skript nach dem Aktivieren des SQLCMD-Modus erneut zu aktivieren, führen Sie folgenden Befehl aus:
+SET NOEXEC OFF; 
+*/
+:setvar __IsSqlCmdEnabled "True"
+GO
+IF N'$(__IsSqlCmdEnabled)' NOT LIKE N'True'
+    BEGIN
+        PRINT N'Der SQLCMD-Modus muss aktiviert sein, damit dieses Skript erfolgreich ausgeführt werden kann.';
+        SET NOEXEC ON;
+    END
+
+
+GO
+USE [$(DatabaseName)];
+
+
+GO
+PRINT N'Sicht "[fakt].[Spot]" wird geändert...';
+
+
+GO
+ALTER VIEW [fakt].[Spot] as 
+
+select 
+
+		h1.Leuchtentyp
+	,	h1.Typ
+	,	h1.Form
+	,	h1.Farbe
+	,	s1.Schutzart
+	,	s1.Einbaudurchmesser	
+	,	s1.Einbautiefe
+	,	s1.Schwenkwinkel
+	,	s1.Produktbeschreibung
+	,	s11.Artikelnummer
+	,	s11.Preis
+	,	s1.Image as Gehaeuse_Image
+	,	h2.Lichtfarbe
+	,	h2.Farbkanal
+	,	h2.Lichtstrom
+	,	h2.Leistung
+	,	h2.Effizienzklasse
+	,	h2.Abstrahlwinkel
+	,	S22.Artikelnummer as Artikelnummer_LED
+	,	S22.Preis as Preis_LED
+	,	s2.Image as Leuchten_Image
+	,	CONCAT(h3.Beschreibung,' ', h3.Versorgung) as Treiber
+	,	h3.Beschreibung
+	,	h3.Versorgung
+	,	h3.Ansteuerung
+	,	CONCAT(UPPER(h3.Ansteuerung), ' - ', (h3.Leistung_Min), 'W', ' ... ' , (h3.Leistung_Max), 'W') as Ansteuerung2
+	,	h3.Ausführung
+	,	s3.Image as Treiber_Image
+	,	h3.Leistung_Min
+	,	h3.Leistung_Max
+	,	s33.Artikelnummer as Artikelnummer_Treiber
+	,	s33.Preis as Preis_Treiber
+	,	CASE WHEN CEILING(h3.Leistung_Min / h2.Leistung) = 0 THEN 1  ELSE CEILING(h3.Leistung_Min / h2.Leistung) END as Min_Anzahl
+	,	Max_Anzahl = FLOOR(h3.Leistung_Max / h2.Leistung)
+
+	,	Lampenplatz = 1
+
+
+
+
+ from hub.Spot_Gehaeuse h1
+ INNER JOIN sat.Spot_Gehaeuse_Stammdaten s1 ON h1.Gehaeuse_SK = s1.Gehaeuse_SK
+ INNER JOIN sat.Spot_Gehaeuse_Artikel s11 ON h1.Gehaeuse_SK = s11.Gehaeuse_SK
+ INNER JOIN Link.Spot_Gehaeuse__Leuchte l1 ON h1.Gehaeuse_SK = l1.Gehaeuse_SK
+ INNER JOIN hub.Spot_Leuchte h2 ON h2.Leuchte_SK = l1.Leuchte_SK
+ INNER JOIN sat.Spot_Leuchte_Stammdaten s2 ON s2.Leuchte_SK = h2.Leuchte_SK
+ INNER JOIN sat.Spot_Leuchte_Artikel s22 ON S22.Leuchte_SK = h2.Leuchte_SK
+ INNER JOIN link.Spot_Leuchte__Treiber l2 ON l2.Leuchte_SK = h2.Leuchte_SK
+ INNER JOIN hub.Spot_Treiber h3 ON l2.Treiber_SK = h3.Treiber_SK
+ INNER JOIN sat.Spot_Treiber_Stammdaten s3 ON h3.Treiber_SK = s3.Treiber_SK
+ inner JOIN sat.Spot_Treiber_Artikel s33 ON h3.Treiber_SK = s33.Treiber_SK
+
+--where 1=1
+----- Gehäuse
+--and typ = '1964'
+--and Farbe = 'chrom'
+--
+---- LED
+--and Farbkanal = 'Statisch'
+--and Lichtfarbe = '2700 K'
+--And h2.Leistung = 3.00
+--
+---- Treiber
+--and Ansteuerung ='DALI-2'
+--and h3.Ausführung = 'Plug & Play'
+GO
+PRINT N'Prozedur "[dbo].[load_link_M1_Gehaeuse__Leuchte]" wird geändert...';
+
+
+GO
+ALTER PROCEDURE [dbo].[load_link_M1_Gehaeuse__Leuchte] as
+
+TRUNCATE TABLE link.M1_Gehaeuse__Leuchte
+
+INSERT INTO link.M1_Gehaeuse__Leuchte (Gehaeuse_SK, Leuchte_SK)
+
+SELECT 
+
+		h1.Gehaeuse_Sk
+	,	h2.Leuchte_SK
+
+FROM hub.M1_Gehaeuse h1
+
+CROSS JOIN (SELECT * FROM hub.M1_Leuchte h2 WHERE Leuchte_SK NOT IN (4,8,12,15,18,28,29)) h2
+
+
+
+
+ -- Ausschluss LED
+--MR 16 STATISCH 2700 K 12 W 38°
+--MR 16 STATISCH 3000 K 12 W 38°
+--MR 16 STATISCH 3500 K 12 W 38°
+--MR 16 STATISCH 4000 K 12 W 38°
+--MR 16 D2W 3000…1800 K 12 W 38°
+--MR 16 SUNLIKE 3000 K 12,5 W 38°
+--MR 16 RGB RGB 13,8 W 45°
+--MR 16 DIM4COLOUR RGB+WW 8 W 60°
+GO
+PRINT N'Update abgeschlossen.';
+
+
+GO
