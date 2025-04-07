9dac17534b6c20e965cf001ecd0bd861994a0367
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:27 +0200

    Copy DataVault/Database1/Database1/sat/Tables/biro40/Biro40_Optik_Stammdaten.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/biro40/Biro40_Optik_Stammdaten.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [sat].[Biro40_Optik_Stammdaten] (
+    [Optik_SK]             INT             NOT NULL,
+    [Kennung]              VARCHAR (512)   NULL,
+    [Bezeichnung]          VARCHAR (512)   NULL,
+    [Artikelnummer]        VARCHAR (512)   NULL,
+    [Preis]                DECIMAL (18, 5) NULL,
+    [Berechnungsfaktor]    INT             NULL,
+    [Ausschreibungstext 1] VARCHAR (512)   NULL,
+    [Gesamtgewicht kg/m]   VARCHAR (512)   NULL,
+    PRIMARY KEY CLUSTERED ([Optik_SK] ASC)
+);
+
