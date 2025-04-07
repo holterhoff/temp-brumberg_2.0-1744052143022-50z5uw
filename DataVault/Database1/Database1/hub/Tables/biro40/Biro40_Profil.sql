fe4e2e113f8787f6682da77cb57f75b8c866b126
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:44 +0200

    Copy DataVault/Database1/Database1/hub/Tables/biro40/Biro40_Profil.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/biro40/Biro40_Profil.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [hub].[Biro40_Profil] (
+    [Profil_SK]              INT           IDENTITY (1, 1) NOT NULL,
+    [Profiltyp]              VARCHAR (512) NULL,
+    [Montageart]             VARCHAR (512) NULL,
+    [Lichtrichtung]          VARCHAR (512) NULL,
+    [Montageart_Kurztext]    VARCHAR (512) NULL,
+    [Anfangswinkel]          INT           NULL,
+    [Endwinkel]              INT           NULL,
+    [Schnittmass_Max]        INT           NULL,
+    [Schnitttyp]             VARCHAR (512) NULL,
+    [Gehaeusefarbe]          VARCHAR (512) NULL,
+    [Gehaeusefarbe_Kurztext] VARCHAR (512) NULL,
+    CONSTRAINT [PK_hub_Biro40_Profil] PRIMARY KEY CLUSTERED ([Profil_SK] ASC)
+);
+
+
+
