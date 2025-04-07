b9ed165a42a4fe758f8d900c2a8f85fce3dd4b8d
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:46 +0200

    Copy DataVault/Database1/Database1/hub/Tables/led/led.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/led/led.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [hub].[LED] (
+    [Leuchte_SK]     INT           IDENTITY (1, 1) NOT NULL,
+    [Leuchtentyp]    VARCHAR (100) NOT NULL,
+    [Typ]            NVARCHAR (50) NOT NULL,
+    [Farbkanal]      VARCHAR (100) NOT NULL,
+    [Lichtfarbe]     VARCHAR (100) NOT NULL,
+    [Versorgung]     VARCHAR (100) NOT NULL,
+    [Leistung]       VARCHAR (100) NOT NULL,
+    [Abstrahlwinkel] VARCHAR (100) NOT NULL,
+    [Lichtstrom]     VARCHAR (100) NOT NULL
+);
+
+
+
