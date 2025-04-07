97283c0e9a5df9b651c62de203105db2258f39f1
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:37 +0200

    Copy DataVault/Database1/Database1/sat/Tables/treiber/Treiber_Stammdaten.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/treiber/Treiber_Stammdaten.sql
@@ -0,0 +1,24 @@
+﻿CREATE TABLE [sat].[Treiber_Stammdaten] (
+    [Treiber_SK]             INT            NOT NULL,
+    [Image]                  VARCHAR (512)  NULL,
+    [Produktbeschreibung_DE] VARCHAR (MAX)  NULL,
+    [Produktbeschreibung_EN] VARCHAR (MAX)  NULL,
+    [SEO_Url_DE]             VARCHAR (MAX)  NULL,
+    [SEO_Url_EN]             VARCHAR (MAX)  NULL,
+    [Artikelnummer]          VARCHAR (512)  NULL,
+    [Preis]                  DECIMAL (5, 2) NULL,
+    [Nettogewicht]           VARCHAR (10)   NULL,
+    [Laenge]                 VARCHAR (50)   NULL,
+    [Breite]                 VARCHAR (50)   NULL,
+    [Hoehe]                  VARCHAR (50)   NULL,
+    [Aussendurchmesser]      VARCHAR (512)  NULL,
+    [Leistungsfaktor]        VARCHAR (512)  NULL,
+    [K_Prüfzeichen]          VARCHAR (50)   NULL,
+    [Datenblatt_DE]          VARCHAR (MAX)  NULL,
+    [Datenblatt_EN]          VARCHAR (MAX)  NULL,
+    [Status]                 VARCHAR (50)   NULL,
+    PRIMARY KEY CLUSTERED ([Treiber_SK] ASC)
+);
+
+
+
