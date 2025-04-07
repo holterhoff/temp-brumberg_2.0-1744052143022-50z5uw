394ec97014c239a26eec4b8ada67454711b4e895
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:41 +0200

    Copy DataVault/Database1/Database1/hub/Tables/biro40/Biro40_GT.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/biro40/Biro40_GT.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [hub].[Biro40_GT] (
+    [GT_SK]         INT           IDENTITY (1, 1) NOT NULL,
+    [Typ]           VARCHAR (512) NULL,
+    [Lichtrichtung] VARCHAR (512) NULL,
+    [Lichtfarbe]    VARCHAR (512) NULL,
+    [Schaltungsart] VARCHAR (512) NULL,
+    [Step]          INT           NULL,
+    CONSTRAINT [PK_hub_Biro40_GT] PRIMARY KEY CLUSTERED ([GT_SK] ASC)
+);
+
