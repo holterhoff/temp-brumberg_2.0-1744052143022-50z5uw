923b51eb7349a75ca3c8280cee5ec1820302d668
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:06:21 +0200

    Copy DataVault/Database1/Database1/sat/Tables/Biro40_Zubehoer_Stammdaten.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/sat/Tables/Biro40_Zubehoer_Stammdaten.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [sat].[Biro40_Zubehoer_Stammdaten] (
+    [Zubehoer_SK]         INT             NOT NULL,
+    [Bezeichnung]         VARCHAR (512)   NULL,
+    [Artikelnummer]       VARCHAR (512)   NULL,
+    [Preis]               DECIMAL (18, 5) NULL,
+    [Material]            VARCHAR (512)   NULL,
+    [ME_Pack]             INT             NULL,
+    [Preisfaktor]         VARCHAR (512)   NULL,
+    [Ausschreibungstext1] VARCHAR (512)   NULL,
+    [Gewicht]             VARCHAR (512)   NULL,
+    [Datenblatt_URL]      VARCHAR (512)   NULL,
+    PRIMARY KEY CLUSTERED ([Zubehoer_SK] ASC)
+);
+
