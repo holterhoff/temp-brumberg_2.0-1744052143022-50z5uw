f4ef68a65ec854728e3184e10c467fea3a06549e
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:23:46 +0200

    Copy src/pages/api/flex/getLichtfarben.js from brumberg_2.0

---
+++ src/pages/api/flex/getLichtfarben.js
@@ -0,0 +1,33 @@
+import query from '../dbservice'
+
+export default async function getLichtfarben(req, res) {
+ if(req.method == 'POST'){
+
+   let params = JSON.parse(req.body)
+   let leuchte =''
+
+
+   params.forEach(element => {
+        if(element.label == 'Leuchte'){
+          leuchte = element.value
+
+        }
+    });
+
+   let sql = "SELECT DISTINCT Lichtfarbe as [value] FROM test WHERE Leuchtentyp LIKE'%"+leuchte+"%'";
+
+   const result = await query(sql)
+
+  if (result) {
+    res.status(200).json(result)
+  }else {
+
+    res.status(500).json({message:'Fehler beim Login'})
+
+  }
+ }else {
+
+    res.status(401).json({message: 'This Methode not allowed'})
+
+ }
+}
