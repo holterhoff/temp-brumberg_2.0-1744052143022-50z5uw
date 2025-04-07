91a2e1d64465acc8da58066d67f5e6964496825d
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:25:24 +0200

    Copy src/pages/api/spot/gehaeuse/getImageGehaeuse.js from brumberg_2.0

---
+++ src/pages/api/spot/gehaeuse/getImageGehaeuse.js
@@ -0,0 +1,44 @@
+'use server'
+
+import { neon } from '@neondatabase/serverless'
+
+const sql = neon(process.env.DATABASE_URL)
+
+export default async function getImageGehaeuse(req, res) {
+  if (req.method == 'POST') {
+    let params = JSON.parse(req.body)
+
+    let query = 'SELECT DISTINCT gehaeuse_image as value FROM spot WHERE 1=1 '
+
+    params.forEach(element => {
+      if (element.label == 'Form') {
+        let form = element.value
+        query += " AND (form_de = '" + form + "' OR form_en = '" + form + "')"
+      }
+      if (element.label == 'Einbautiefe') {
+        let einbautiefe = element.value
+        query += " AND einbautiefe = '" + einbautiefe + "'"
+      }
+      if (element.label == 'Typ') {
+        let typ = element.value
+        query += " AND (typ_gehaeuse_de = '" + typ + "' OR typ_gehaeuse_en = '" + typ + "')"
+      }
+      if (element.label == 'Gehäusefarbe') {
+        let gehaeusefarbe = element.value
+        query += " AND ([Farbe_DE] = '" + gehaeusefarbe + "' OR [Farbe_EN] = '" + gehaeusefarbe + "')"
+      }
+    })
+
+    query += ' ORDER BY 1'
+
+    const result = await sql(query)
+
+    if (result) {
+      res.status(200).json(result)
+    } else {
+      res.status(500).json({ message: 'Fehler beim Login' })
+    }
+  } else {
+    res.status(401).json({ message: 'This Methode not allowed' })
+  }
+}
