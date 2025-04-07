a2e68ed260d7b7b5c6e62f4fe06f359b598838a1
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:28:06 +0200

    Copy src/pages/konfigurator/datasheet/preview/index.js from brumberg_2.0

---
+++ src/pages/konfigurator/datasheet/preview/index.js
@@ -0,0 +1,8 @@
+// ** Demo Components Imports
+import Preview from 'src/views/konfigurator/datasheet/preview/Preview'
+
+const InvoicePreview = () => {
+  return <Preview id='1-1-1' />
+}
+
+export default InvoicePreview
