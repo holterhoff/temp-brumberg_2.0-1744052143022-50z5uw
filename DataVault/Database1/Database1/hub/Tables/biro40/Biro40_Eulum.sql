0d1062aa42ca8d27a1fb983472494e6ea5feae6d
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:40 +0200

    Copy DataVault/Database1/Database1/hub/Tables/biro40/Biro40_Eulum.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/biro40/Biro40_Eulum.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [hub].[Biro40_Eulum] (
+    [Eulum_SK]         INT           IDENTITY (1, 1) NOT NULL,
+    [Typ]              VARCHAR (512) NULL,
+    [Lichtrichtung]    VARCHAR (512) NULL,
+    [Lichtfarbe]       VARCHAR (512) NULL,
+    [Schaltungsart]    VARCHAR (512) NULL,
+    [Optisches_System] VARCHAR (512) NULL,
+    [Min]              INT           NULL,
+    [Max]              INT           NULL,
+    [Step]             INT           NULL,
+    [Ecklaenge]        INT           NULL,
+    [Eckmodus]         VARCHAR (512) NULL,
+    CONSTRAINT [PK_hub_Biro40_Eulum] PRIMARY KEY CLUSTERED ([Eulum_SK] ASC)
+);
+
