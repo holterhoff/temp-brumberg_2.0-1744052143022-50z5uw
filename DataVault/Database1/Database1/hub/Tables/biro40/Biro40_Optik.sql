79d2b5a87fe70568082cf37da085105d2eb29412
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 21:05:42 +0200

    Copy DataVault/Database1/Database1/hub/Tables/biro40/Biro40_Optik.sql from brumberg_2.0

---
+++ DataVault/Database1/Database1/hub/Tables/biro40/Biro40_Optik.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [hub].[Biro40_Optik] (
+    [Optik_SK]                  INT           IDENTITY (1, 1) NOT NULL,
+    [Typ]                       VARCHAR (512) NULL,
+    [Lichtrichtung]             VARCHAR (512) NULL,
+    [Einsatz]                   VARCHAR (512) NULL,
+    [Anfangswinkel]             INT           NULL,
+    [Endwinkel]                 INT           NULL,
+    [Schnittmass_Max]           INT           NULL,
+    [Schnitttyp]                VARCHAR (512) NULL,
+    [Optisches_System]          VARCHAR (512) NULL,
+    [Optisches_System_Kurztext] VARCHAR (512) NULL,
+    CONSTRAINT [PK_hub_Biro40_Optik] PRIMARY KEY CLUSTERED ([Optik_SK] ASC)
+);
+
+
+
