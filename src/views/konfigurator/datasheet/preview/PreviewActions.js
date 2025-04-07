b52daeeee74d7a870c3bd5761d6c0df23ae78722
holterhoff <markus.holterhoff@googlemail.com>
2025-04-07 22:42:12 +0200

    Copy src/views/konfigurator/datasheet/preview/PreviewActions.js from brumberg_2.0

---
+++ src/views/konfigurator/datasheet/preview/PreviewActions.js
@@ -0,0 +1,46 @@
+// ** Next Import
+import Link from 'next/link'
+
+// ** MUI Imports
+import Card from '@mui/material/Card'
+import Button from '@mui/material/Button'
+import CardContent from '@mui/material/CardContent'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+const PreviewActions = ({ id, toggleSendInvoiceDrawer, toggleAddPaymentDrawer }) => {
+  return (
+    <Card>
+      <CardContent>
+        <Button
+          fullWidth
+          target='_blank'
+          sx={{ mb: 3.5 }}
+          component={Link}
+          color='secondary'
+          variant='outlined'
+          href={`/konfigurator/datasheet/print/${id}`}
+        >
+          Print
+        </Button>
+        <Button fullWidth sx={{ mb: 3.5 }} color='secondary' variant='outlined'>
+          Download
+        </Button>
+
+        <Button
+          fullWidth
+          sx={{ mb: 3.5 }}
+          component={Link}
+          color='secondary'
+          variant='outlined'
+          href={`/konfigurator/invoice/edit/${id}`}
+        >
+          Edit Invoice
+        </Button>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default PreviewActions
